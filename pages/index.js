1d66a81e0519d6a6008e46f6c2ecb01585b581b7
Fábio Santos <delith@gmail.com>
2023-08-02 23:31:16 +0000

    Teste com document.write 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return document.write("<h1>Sandra, eu amo você!! <br> E também sinto sua falta!</h1>");
+    return document.write("<h1>Sandra, eu amo você!!  E também sinto sua falta!</h1>");
 }
 
 export default Home;
