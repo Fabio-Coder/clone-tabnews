71300394983535d4740767713c30a752cf76167e
Fábio Santos <delith@gmail.com>
2023-08-02 23:23:32 +0000

    Mais uma alteração da mensagem!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sandra, eu amo você!!!<br>Também sinto muito sua falta!!!</br></h1>
+    return <h1>Sandra, eu amo você!!!<br>Também sinto muito sua falta!!!</h1>
 }
 
 export default Home;
